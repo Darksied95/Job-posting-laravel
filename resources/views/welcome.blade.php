fede0f265630cb71e1b1c8bf808b4e0571f1eac2
Aliyu Efe <aliyuefe9501@gmail.com>
2024-09-04 11:44:11 +0100

    update ui

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,13 @@
 <x-layout>
     <div class="space-y-10">
 
-        <section>
+        <section class="text-center pt-6">
+            <h1 class="font-bold text-4xl ">Let's Find Your Next Job</h1>
+            <form action="">
+                <input type="text" placeholder="Web Developer..." class="rounded-xl bg-white/5 border-white/10 px-5 py-4 w-full max-w-xl">
+            </form>
+        </section>
+        <section class="pt-10">
             <x-section-heading>Featured Jobs</x-section-heading>
             <div class="grid  lg:grid-cols-3 gap-8 mt-6">
                 <x-job-card> </x-job-card>
@@ -9,6 +15,7 @@
                 <x-job-card> </x-job-card>
             </div>
         </section>
+
         <section>
             <x-section-heading>Tags</x-section-heading>
 
