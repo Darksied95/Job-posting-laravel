b7d115bc22bb9aea80e0dafc9b86089e581db0a4
Aliyu Efe <aliyuefe9501@gmail.com>
2024-09-06 23:46:51 +0100

    add search functionalities

--- resources/views/Job/index.blade.php
+++ resources/views/Job/index.blade.php
@@ -2,11 +2,12 @@
     <div class="space-y-10">
 
         <section class="text-center pt-6">
-            <h1 class="font-bold text-4xl ">Let's Find Your Next Job</h1>
-            <form action="">
-                <input type="text" placeholder="Web Developer..."
-                    class="rounded-xl bg-white/5 border-white/10 px-5 py-4 w-full max-w-xl">
-            </form>
+            <h1 class="font-bold text-4xl mb-3">Let's Find Your Next Job</h1>
+
+            <x-forms.form action="/search" class="mt-6">
+                <x-forms.input placeholder="Web Developer..." name="q" :label="false" />
+            </x-forms.form>
+            
         </section>
         <section class="pt-10">
             <x-section-heading>Featured Jobs</x-section-heading>
