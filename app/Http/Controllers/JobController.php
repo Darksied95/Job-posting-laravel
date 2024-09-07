5a73d85312001b97458ab6d600ac68a37968d749
Aliyu Efe <aliyuefe9501@gmail.com>
2024-09-07 01:15:38 +0100

    update job controller

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -2,71 +2,73 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreJobRequest;
 use App\Http\Requests\UpdateJobRequest;
 use App\Models\Job;
 use App\Models\Tag;
+use Arr;
+use Auth;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
 
 class JobController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+ 
     public function index()
     {
-        $jobs = Job::all()->groupBy('featured');
+        $jobs = Job::latest()->get()->groupBy('featured');
+
         return view("Job.index", [
-            'featuredJobs' => $jobs[0],
-            'jobs' => $jobs[1],
+            'featuredJobs' => $jobs[1],
+            'jobs' => $jobs[0],
             'tags' => Tag::all(),
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        //
+        return view('Job.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreJobRequest $request)
+    public function store(Request $request)
     {
-        //
-    }
+        $attributes = $request->validate([
+            'title' => ['required'],
+            'salary' => ['required'],
+            'location' => ['required'],
+            'schedule' => ['required', Rule::in(['Part Time', 'Full Time', 'Contract'])],
+            'url' => ['required', 'active_url'],
+            'tags'=> ['nullable'],
+        ]);
 
-    /**
-     * Display the specified resource.
-     */
+        $attributes['featured'] = $request->has('featured');
+
+        $job = Auth::user()->employer->jobs()->create(Arr::except($attributes, 'tags'));
+
+        if($attributes['tags'] ?? false){
+            foreach(explode(',', $attributes['tags']) as $tag){
+                $job->tag($tag);
+            }
+        }
+        return redirect('/');
+     }
     public function show(Job $job)
     {
-        //
+        
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Job $job)
     {
-        //
+        
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(UpdateJobRequest $request, Job $job)
     {
-        //
+        
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Job $job)
     {
-        //
+        
     }
 }
